ea5b2bb38f8813ddb5470d430167e5a422ab4a96
kevinadhiguna <kevin26798@gmail.com>
2020-10-24 23:55:35 +0700

    refactor: add mongoose connection

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/subscribers');
+mongoose.connect("mongodb://localhost/subscribers", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
-app.listen(6000, () => console.log('Server is running on port 6000'));
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("🔌 Successfully connected to MongoDB"));
+
+app.listen(6000, () => console.log("🚀 Server is running on port 6000"));
