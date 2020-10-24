6126a84dee51a44fa881967e33b8845d53eaa353
kevinadhiguna <kevin26798@gmail.com>
2020-10-25 00:50:28 +0700

    feat: add CRUD action on API

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,41 +1,94 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Subscriber = require('../models/subscriber');
+const Subscriber = require("../models/subscriber");
 
 // == CRUD Actions ==
 
 // [1] Read/Retrive
 // Getting all
-router.get('/', (req, res) => {
-    res.send('Hello World');
+router.get("/", async (req, res) => {
+	// Try-Catch is used here to tell a user what happened if an error occurs
+	try {
+		const subscribers = await Subscriber.find();
+		// Send 'subscribers' to the user if succeed
+		res.json(subscribers);
+	} catch (err) {
+		// Set status to '500' on error to notify a user that something is wrong with server.. (Server Error)
+		res.status(500).json({ message: err.message });
+	}
 });
 
 // Getting One
-router.get('/:id', (req, res) => {
-
+router.get("/:id", (req, res) => {
+	// Send a response to a user based on what he/she wants to request (find)
+	res.send(req.params.id);
 });
 
 // [2] Create
 // Creating One
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
+	// Pass a new subscriber according to what a user requested
+	const subscriber = new Subscriber({
+		name: req.body.name,
+		subscribedToChannel: req.body.subscribedToChannel,
+	});
 
+	try {
+		const newSubscriber = await subscriber.save();
+		// status '201' means a user has successfully created something new. More specific than '200'
+		res.status(201).json(newSubscriber);
+	} catch (err) {
+		// An error mainly occurs here when a user does not pass some required fields such as subscriber's name. Hence the error will be on client-side (status: 4XX).
+		res.status(400).json({ message: err.message });
+	}
 });
 
 // [3] Update
 
-// Note: 
+// Note:
 // -> I use 'patch' instead of 'put' because I want to update only based on what user passed.
 // -> On the other hand, 'put' changes all existing fields even if a user only changes one field.
 
 // Updating One
-router.patch('/:id', (req, res) => {
+router.patch("/:id", getSubscriber, async (req, res) => {
+	if (req.body.name !== null) {
+		res.subscriber.name = req.body.name;
+	}
+	if (req.body.subscribedToChannel !== null) {
+		res.subscriber.subscribedToChannel = req.body.subscribedToChannel;
+	}
 
+	try {
+		const updatedSubscriber = await res.subscriber.save();
+		res.json(updatedSubscriber);
+	} catch (err) {
+		res.status(400).json({ message: "err.message" });
+	}
 });
 
 // [4] Delete
 // Deleting One
-router.delete('/:id', (req, res) => {
-
+router.delete("/:id", getSubscriber, async (req, res) => {
+try {
+    await res.subscriber.remove();
+    res.json({message: 'Successfully deleted a subscriber!'})
+} catch (err) {
+    res.status(500).json({message: err.message});    
+}
 });
+
+function getSubscriber(req, res, next) {
+	try {
+		subscrier = await Subscriber.findById(req.params.id);
+		if (subscrier == null) {
+			return res.status(404).json({ message: "Can not find Subscribers" });
+		}
+	} catch (err) {
+		return res.status(500).json({ message: err.message });
+	}
+
+	res.subscriber = subscriber;
+	next();
+}
 
 module.exports = router;
