51a12e46f722ef41595a445e58531ffaef3cfcf4
kevinadhiguna <kevin26798@gmail.com>
2020-10-25 00:20:46 +0700

    refactor: add subscriber model

---
+++ models/subscriber.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+// Create a Schema
+const subscriberSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    subscribedToChannel: {
+        type: String,
+        required: true,
+    },
+    subscribedDate: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Subscriber', subscriberSchema);
